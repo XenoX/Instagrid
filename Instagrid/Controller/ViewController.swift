7434d77583a4e63a374dc64ffd1fc4029a945d4a
XenoX <xenox1337@gmail.com>
2019-03-15 20:58:37 +0100

    Handle Landcape

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -14,17 +14,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setStyle()
     }
 
     private func resetLayoutButtons() {
         layoutButtons.forEach { layoutButton in
-            layoutButton.setImage(nil, for: .normal)
             layoutButton.isSelected = false
+            layoutButton.setImage(nil, for: .normal)
         }
     }
 
     private func selectLayoutButton(_ layoutButton: UIButton) {
         layoutButton.isSelected = true
         layoutButton.setImage(UIImage.init(named: "Selected"), for: .selected)
+    }
+
+    private func setStyle() {
+        imageGridView.layer.shadowColor = UIColor.black.cgColor
+        imageGridView.layer.shadowOffset = CGSize(width: 0, height: 2)
+        imageGridView.layer.shadowOpacity = 0.8
     }
 }
